53fd6b1a7e67f31c77b354c3aa96282057c77c80
Jagannath <jagannath.bhanja@ioptechnologies.com>
2025-02-08 23:12:18 +0530

    new bootstrap changes and login page

--- ProManageApp/views.py
+++ ProManageApp/views.py
@@ -1,3 +1,20 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
+
+@csrf_exempt
+def index(request):
+    try:
+        return render(request, 'ProManageTemplates/index.html')
+    except Exception as e:
+        print("Exception at index --> ", e)
+        return HttpResponseRedirect('/')
+@csrf_exempt
+def login(request):
+    try:
+        return render(request, 'ProManageTemplates/login.html')
+    except Exception as e:
+        print("Exception at index --> ", e)
+        return HttpResponseRedirect('/')
